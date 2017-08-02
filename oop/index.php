bf7c24358cc31c88631074f4a62c48a82cafaabc
William Arevalo <willi11-95@hotmail.com>
2017-08-02 13:59:21 -0500

    Intefaces y serialize y unseralize
    Repasar

--- oop/index.php
+++ oop/index.php
@@ -7,9 +7,8 @@ include 'vehicles/Truck.php';
 //use Vehicles\Truck;
 use Vehicles\{Truck, Car};
 
+
 echo 'Class Car: <br>';
-
-
 $car = new Car('Juakin'); //DEclaro un objeto de tipo Car
 $car->move(); //metodo de su propia clase
 //echo 'Owner car 1: ' . $car->getOwner();
@@ -32,4 +31,16 @@ echo '<br>Total Trucks: ' . Truck::getTotal() . '<br>';
 //Demostrando que no es bueno tener un clase vehicle
 //$v1 = new \Vehicles\VehicleBase('Alex');
 //$v1->move();
+
+
+/*Interfaces!!!
+	permiten definir que metodos deben implementar ciertas clases sin necesidad de decir como deben ser implementadas. 
+	Es decir agregar las firmas de las funciones y de esa forma sabremos que necesita implementar el metodo
+	a diferencia de las clases abstractas si pueden tener contenido en cambio la interfaz no.
+	actuara como otro objeto.
+*/
+$ser = serialize($car);
+$newCar = unserialize($ser);
+
+echo 'NewCar Owner '. $newCar->getOwner() . '<br>';
 ?>
--- oop/vehicles/Car.php
+++ oop/vehicles/Car.php
@@ -4,7 +4,7 @@ namespace Vehicles;
 
 require_once 'VehicleBase.php';
 
-class Car extends VehicleBase
+class Car extends VehicleBase implements \Serializable
 {
 	//public function move(){
 	//	echo 'Car: moving<br>';
@@ -12,6 +12,17 @@ class Car extends VehicleBase
 	public function startEngine(){
 		return 'Car: start engine';
 	}
+
+	public function serialize(){
+		echo 'Serialize<br>';
+		return $this->owner;
+	}
+	public function unserialize($serialized){
+		echo 'unserialize<br>';
+		$this->owner = $serialized;
+	}
+
 }
+
 
 ?>
