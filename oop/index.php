5f8eecaca25f4fcece4c95eff53fb44802964a3b
William Arevalo <willi11-95@hotmail.com>
2017-08-02 10:24:52 -0500

    Static
    Creo un contador sobre la clase que no afectara a la clase si tocar las instancias.
    private static count, accedo por un metodo public static  y retorno self::$count , luego invoco el metoddo de la sgte forma Truck::getTotal(); (Clase::metodo())

--- oop/index.php
+++ oop/index.php
@@ -12,12 +12,18 @@ echo 'Class Car: <br>';
 
 $car = new Car('Juakin'); //DEclaro un objeto de tipo Car
 $car->move(); //metodo de su propia clase
-echo 'Owner car 1: ' . $car->getOwner();
+//echo 'Owner car 1: ' . $car->getOwner();
 echo '<br><br>';
-echo 'Class truck: <br>';
 
+echo 'Class truck 1: <br>';
 $truck = new Truck('Will', 'Pickup');
 $truck->move(); //metodo de su clase
-echo 'Owner Truck: ' . $truck->getOwner();
+//echo 'Owner Truck: ' . $truck->getOwner();
 
+echo 'Class truck 2: <br>';
+$truck = new Truck('German', 'Pickup');
+$truck->move(); //metodo de su clase
+
+
+echo '<br>Total Trucks: ' . Truck::getTotal() . '<br>';
 ?>
--- oop/vehicles/Truck.php
+++ oop/vehicles/Truck.php
@@ -6,18 +6,25 @@ require_once 'VehicleBase.php';
 
 class Truck extends VehicleBase
 {
+
+	private static $count = 0; //Static es para ser accesible sin instanciar la clase, se utiliza con metodos y propiedas, es decir, si pongo el contador afectara a todos los objetos con esa clase.
+
 	private $type;
 
 	public function __construct($ownerName, $type){
 		
 		$this->type = $type;
 		$this->owner = $ownerName;
+		self::$count++;
 		//parent::__construct($ownerName); //No se llama efectivamente a la clase padre y al constructor si no lleva parent::
 
 		// estamos utilizando este constructor y no el de arriba por lo tanto no sale el mensjae de constructor
 	}
 	public function move(){
 		echo 'Truck: ' . $this->type . ' moving<br>';
+	}
+	public static function getTotal(){
+		return self::$count;
 	}
 }
 
