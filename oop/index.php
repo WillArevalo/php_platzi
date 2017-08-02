d4dc0e2a52545557a0750eb478fa667fa8369489
William Arevalo <willi11-95@hotmail.com>
2017-08-02 09:26:26 -0500

    Termino de agregar herencia
    Cambie para que el construcotr del truck inicie en su clase y no en la padre

--- oop/index.php
+++ oop/index.php
@@ -3,9 +3,9 @@
 /**
 * 
 */
-class Car
+class Vehicle
 {
-	private $owner;
+	protected $owner; // privada solo se puede acceder atravez de metodo get dentro de la misma clase, con protected y es accesible atravez de su clase o clases hijas.
 
 	public function move(){
 		echo '<br>moving<br>';
@@ -19,25 +19,56 @@ class Car
 		$this->owner = $owner;
 	}
 
+	//Constructor y destructor
 	public function __construct($ownerName){
 		$this->owner = $ownerName;
 		echo 'constructor<br>';
 	}
 	public function __destruct(){
-		echo '<br>destruct';
+		echo '<br><br>destruct';
 	}
 
 }
+
+//Herencia
+/**
+* 
+*/
+class Car extends Vehicle
+{
+	public function move(){
+		echo 'Car: moving<br>';
+	}
+}
+
+class Truck extends Vehicle
+{
+	private $type;
+
+	public function __construct($ownerName, $type){
+		
+		$this->type = $type;
+		$this->owner = $ownerName;
+		//parent::__construct($ownerName); //No se llama efectivamente a la clase padre y al constructor si no lleva parent::
+
+		// estamos utilizando este constructor y no el de arriba por lo tanto no sale el mensjae de constructor
+	}
+	public function move(){
+		echo 'Truck: ' . $this->type . ' moving<br>';
+	}
+}
+
 echo 'Class Car: <br>';
 
 
-$car = new Car('Max'); //DEclaro un objeto de tipo Car
-$car2 = new Car('Will');
-
-$car->move();
-
+$car = new Car('Juakin'); //DEclaro un objeto de tipo Car
+$car->move(); //metodo de su propia clase
 echo 'Owner car 1: ' . $car->getOwner();
-echo '<br>';
-echo 'Owner car 2: ' . $car2->getOwner();
+echo '<br><br>';
+echo 'Class truck: <br>';
+
+$truck = new Truck('Will', 'Pickup');
+$truck->move(); //metodo de su clase
+echo 'Owner Truck: ' . $truck->getOwner();
 
 ?>
